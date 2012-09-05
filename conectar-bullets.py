7bc27ae54d7e57a4c532375ee15ab51c5ade4206
Miguel Angel Garcia <miguel.garcia@gmail.com>
2012-09-05 00:46:02 -0300

    commit inicial

---
+++ conectar-bullets.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import gtk
+import gtk.gdk
+import webkit 
+import os
+import sys
+import subprocess
+
+class BulletsBrowser(webkit.WebView):
+	def __init__(self):
+		webkit.WebView.__init__(self)
+		self.get_property("settings").set_property("enable-default-context-menu", False)
+		self.connect('navigation-policy-decision-requested', self._on_navigate_decision)
+		self.load_uri("file://" + os.path.dirname(os.path.abspath(__file__)) + "/1.html")
+    
+	def _on_navigate_decision(self, view, frame, req, action, decision):
+		parts =  req.get_uri().split("://")
+		if len(parts) == 2:
+			if parts[0] == 'exec':
+				subprocess.Popen(parts[1])
+				return True
+			if parts[0] == 'ui' and parts[1] == 'close':
+				gtk.main_quit()
+				return True
+		return False
+
+if __name__ == '__main__':
+	sw = gtk.ScrolledWindow()
+	bullet_browser = BulletsBrowser() 
+	sw.add(bullet_browser) 
+	win = gtk.Window(gtk.WINDOW_TOPLEVEL)
+	win.add(sw) 
+	win.set_resizable(False)
+	bullet_browser.set_size_request(600, 400)
+	win.set_default_size(600, 400)
+	win.set_title("Primeros pasos")
+	win.set_position(gtk.WIN_POS_CENTER)
+	win.set_deletable(False)
+	win.show_all() 
+
+	win.connect("destroy", gtk.main_quit)
+	gtk.main()
