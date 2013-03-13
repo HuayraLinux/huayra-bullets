06f43c7d1d8b2221a6ae465926c4576e7837d35d
Luciano Baraglia <luciano.baraglia@gmail.com>
2013-03-13 13:24:44 -0300

    Modificación textos easter egg

--- assets/js/eggs.js
+++ assets/js/eggs.js
@@ -1,16 +1,22 @@
-// https://github.com/nakajima/jquery-easter-eggs
+/*
+ * Keystrokes event for jQuery
+ * http://www.boedesign.com/
+ */ 
 
 easter_eggs = new Array();
+
 easter_eggs['equipo_huayra'] = "\
 <div class='easter-egg-wrapper'>\
 	<h1>Huayra GNU/Linux</h1>\
 	<div class='personas' id='directores-huayra'>\
+		<h3>Presidenta de la Nación:</h3>\
+		<p>Dra. Cristina Fernández de Kirchner</p>\
 		<h3>Director Ejecutivo de la ANSES:</h3>\
-		<p>Diego Bossio</p>\
+		<p>Lic. Diego Bossio</p>\
 		<h3>Directora Ejecutiva del Programa Conectar Igualdad:</h3>\
-		<p>Silvina Gvirtz</p>\
+		<p>Lic. Silvina Gvirtz</p>\
 		<h3>Directora de Comunicación y Contenidos del Programa Conectar Igualdad:</h3>\
-		<p>Constanza Necuzzi</p>\
+		<p>Lic. Constanza Necuzzi</p>\
 	</div>\
 \
 	<h2>Los integrantes de Huayra somos:</h2>\
@@ -26,10 +32,13 @@ easter_eggs['equipo_huayra'] = "\
 		<h3>Equipo Arte y Diseño:</h3>\
 		<p>Claudio Andaur - Carolina Hortal, Mercedes Elgarte, Iván Hoffmann</p>\
 	</div>\
-	<p>Agradecemos la iniciativa de <strong>Pablo Fontdevila</strong>, quien creyó en este proyecto desde el inicio y a todos los colaboradores de la <strong>comunidad educativa</strong> y del <strong>Software Libre</strong>.\
+	<h3>Agradecemos:</h3>\
+	<p>A <strong>Pablo Fontdevila</strong> que desde el inicio creyó en este proyecto</p>\
+	<p>A <strong>Laura Penacca</strong> por su empuje y trabajo incansable día a día</p>\
+	<p>A todos los colaboradores de la <strong>comunidad educativa</strong></p>\
+	<p>A la comunidad del <strong>Software Libre</strong></p>\
 </div>\
 ";
-
 
 $(document).ready(function(){
 	$(document).bind('keystrokes', {
@@ -39,3 +48,4 @@ $(document).ready(function(){
 					
 	});
 });
+    
